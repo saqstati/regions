0279b3975a27324adb7251d478156f74f6813b31
nikolozi2001 <mr.qachibaia@gmail.com>
2022-09-26 17:27:16 +0400

    ganixiles zemot, chasworebulia satauri

--- list.php
+++ list.php
@@ -6,13 +6,13 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta property="og:url" content="http://www.geostat.ge/regions/" />
     <meta property="og:type" content="website" />
-    <meta property="og:title" content="სტატისტიკური ინფორმაციის შედარება საქართველოს რეგიონების მიხედვით" />
-    <meta property="og:description" content="სტატისტიკური ინფორმაციის შედარება საქართველოს რეგიონების მიხედვით" />
+    <meta property="og:title" content="ძირითადი მაჩვენებლების შედარება საქართველოს რეგიონების მიხედვით" />
+    <meta property="og:description" content="ძირითადი მაჩვენებლების შედარება საქართველოს რეგიონების მიხედვით" />
     <meta property="og:image" content="http://www.geostat.ge/regions/images/regionsbanner1.png" />
     <meta property="og:image:secure_url" content="http://www.geostat.ge/regions/images/regionsbanner1.png" />
     <meta property="og:image:width" content="740" />
     <meta property="og:image:height" content="450" />
-    <title class="tr" Key="PAGE_TITLE">სტატისტიკური ინფორმაციის შედარება საქართველოს რეგიონების მიხედვით</title>
+    <title class="tr" Key="PAGE_TITLE">ძირითადი მაჩვენებლების შედარება საქართველოს რეგიონების მიხედვით</title>
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
     <script src="https://code.jquery.com/jquery-3.2.1.min.js" integrity="sha256-hwg4gsxgFZhOsEEamdOYGBf13FyQuiTwlAQgxVSNgt4=" crossorigin="anonymous"></script>
     <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/gsap/1.19.0/TweenMax.min.js"></script>
@@ -73,7 +73,7 @@
         </div>
         <header class="header1">
             <h1>
-                <p id="pagetitlename" class="tr" Key="REGION14">სტატისტიკური ინფორმაციის შედარება საქართველოს რეგიონების მიხედვით</p>
+                <p id="pagetitlename" class="tr" Key="REGION14">ძირითადი მაჩვენებლების შედარება საქართველოს რეგიონების მიხედვით</p>
             </h1>
         </header>
         <div id="languages">
