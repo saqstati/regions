db7b4f7e9b6f78066bea87abaacb06f71864bc19
nikolozi2001 <mr.qachibaia@gmail.com>
2022-10-03 17:55:23 +0400

    updating

--- script.js
+++ script.js
@@ -30,12 +30,12 @@
 // function addSamegreloClass() {
 //    var element = document.getElementById("group2-zoomed");
 //    element.classList.add("appearElement");
-  // element.style.display="block"
+// element.style.display="block"
 
-  //   var style = document.getElementsByClassName("elementAppear").style.display;
-  //   if (style === "block")
-  //     document.getElementsByClassName("elementAppear").style.display = "none";
-  //   else document.getElementsByClassName("elementAppear").style.display = "block";
+//   var style = document.getElementsByClassName("elementAppear").style.display;
+//   if (style === "block")
+//     document.getElementsByClassName("elementAppear").style.display = "none";
+//   else document.getElementsByClassName("elementAppear").style.display = "block";
 //}
 
 // function HideElement(){
@@ -46,12 +46,82 @@
 
 // }
 
-
-// $(document).ready(function() {    
+// $(document).ready(function() {
 //     $('group2').bind('click', function() {
 //         if ($(this).hasClass('active'))
-           
+
 //         else
-            
+
 //     });
 // });
+
+let parent = document.getElementById("append");
+
+function render(data, index) {
+  console.log(index);
+  var wrapper = document.createElement("tr");
+  wrapper.id = "IdTrInfoRow";
+  wrapper.className = trWrapper[index];
+
+  let html = `
+                <tr id=trInfoRow>
+                    <td class=${tdWrapper[index]}><div class="area">${data.Area}</div></td>
+                    <td class=${tdWrapper[index]}><div class="population">${data.Population}</div></td>
+                    <td class=${tdWrapper[index]}><div class="GDP"> ${data.GDP} </div> </td>
+                    <td class=${tdWrapper[index]}><div class="GDPPerCapita">${data.GDPPerCapita}</div></td>
+                    <td class=${tdWrapper[index]}><div class="UnemploymentRate">${data.UnemploymentRate}</div></td>
+                    <td class=${tdWrapper[index]}><div class="EmploymentRate">${data.EmploymentRate}</div></td>
+                    <td class=${tdWrapper[index]}><div class="EmploymentRateIndustry">${data.EmploymentRateIndustry}</div></td>
+                    <td class=${tdWrapper[index]}><div class="AverageSalaryIndustry">${data.AverageSalaryIndustry}</div></td>
+                    <td class=${tdWrapper[index]}><div class="RegistredEntities">${data.RegistredEntities}</div></td>
+                </tr> 
+            `;
+
+  wrapper.innerHTML = html;
+
+  return wrapper;
+}
+
+let testRequest = function (data) {
+  return axios({
+    method: "post",
+    url: "get_select.php",
+    data: data,
+    headers: {
+      "Content-Type": "multipart/form-data",
+    },
+  })
+    .then(function (response) {
+      parent.innerHTML = "";
+
+      response.data.map(function (i, index) {
+        let dom = render(i, index);
+        parent.append(dom);
+      });
+
+      //handle success
+      console.log(response.data);
+      myFunction();
+      // trInfoRowFunction();
+    })
+    .catch(function (response) {
+      //handle error
+      console.log(response);
+    });
+};
+
+function validateform(e) {
+  e.preventDefault();
+
+  const formData = new FormData(e.target);
+
+  for (var pair of formData.entries()) {
+    console.log(pair[0] + ", " + pair[1]);
+  }
+
+  testRequest(formData);
+}
+
+var form = document.getElementsByName("applyform")[0];
+console.log("form", form);
+form.addEventListener("submit", validateform);
