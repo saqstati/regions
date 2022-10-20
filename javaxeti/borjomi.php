2f4456cac77809acbafc93e4c80c3aee6c02b36d
nikolozi2001 <mr.qachibaia@gmail.com>
2022-10-20 11:30:23 +0400

    finished samtskhe javakheti

--- javaxeti/borjomi.php
+++ javaxeti/borjomi.php
@@ -6,13 +6,13 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta property="og:url" content="http://www.geostat.ge/regions/" />
     <meta property="og:type" content="website" />
-    <meta property="og:title" content="სტატისტიკური ინფორმაცია სამეგრელო-ზემო სვანეთის რეგიონის ბორჯომის შესახებ" />
-    <meta property="og:description" content="სტატისტიკური ინფორმაცია სამეგრელო-ზემო სვანეთის რეგიონის ბორჯომის შესახებ" />
+    <meta property="og:title" content="სტატისტიკური ინფორმაცია სამცხე-ჯავახეთის რეგიონის ბორჯომის შესახებ" />
+    <meta property="og:description" content="სტატისტიკური ინფორმაცია სამცხე-ჯავახეთის რეგიონის ბორჯომის შესახებ" />
     <meta property="og:image" content="http://www.geostat.ge/regions/images/regionsbanner1.png" />
     <meta property="og:image:secure_url" content="http://www.geostat.ge/regions/images/regionsbanner1.png" />
     <meta property="og:image:width" content="740" />
     <meta property="og:image:height" content="450" />
-    <title class="tr" Key="PAGE_TITLE">სტატისტიკური ინფორმაცია სამეგრელო-ზემო სვანეთის რეგიონის ბორჯომის შესახებ</title>
+    <title class="tr" Key="PAGE_TITLE">სტატისტიკური ინფორმაცია სამცხე-ჯავახეთის რეგიონის ბორჯომის შესახებ</title>
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
     <script src="https://code.jquery.com/jquery-3.2.1.min.js" integrity="sha256-hwg4gsxgFZhOsEEamdOYGBf13FyQuiTwlAQgxVSNgt4=" crossorigin="anonymous"></script>
     <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/gsap/1.19.0/TweenMax.min.js"></script>
@@ -50,18 +50,18 @@
                 }
             }
             preload(
-                "1600-900-optimized/2.jpg",
-                "1600-900-optimized/3.jpg",
-                "1600-900-optimized/4.jpg",
-                "1600-900-optimized/5.jpg",
-                "1600-900-optimized/6.jpg",
-                "1600-900-optimized/7.jpg",
-                "1600-900-optimized/8.jpg",
-                "1600-900-optimized/9.jpg",
-                "1600-900-optimized/10.jpg",
-                "1600-900-optimized/11.jpg",
-                "1600-900-optimized/12.jpg",
-                "1600-900-optimized/15.jpg"
+                "../1600-900-optimized/2.jpg",
+                "../1600-900-optimized/3.jpg",
+                "../1600-900-optimized/4.jpg",
+                "../1600-900-optimized/5.jpg",
+                "../1600-900-optimized/6.jpg",
+                "../1600-900-optimized/7.jpg",
+                "../1600-900-optimized/8.jpg",
+                "../1600-900-optimized/9.jpg",
+                "../1600-900-optimized/10.jpg",
+                "../1600-900-optimized/11.jpg",
+                "../1600-900-optimized/12.jpg",
+                "../1600-900-optimized/15.jpg"
             )
         </script>
     </div>
@@ -72,7 +72,7 @@
         </div>
         <header class="header1">
             <h1>
-                <p id="pagetitlename" class="tr" Key="REGION14">სტატისტიკური ინფორმაცია სამეგრელო-ზემო სვანეთის რეგიონის ბორჯომის შესახებ</p>
+                <p id="pagetitlename" class="tr" Key="REGION14">სტატისტიკური ინფორმაცია სამცხე-ჯავახეთის რეგიონის ბორჯომის შესახებ</p>
             </h1>
         </header>
         <div id="languages">
@@ -96,7 +96,7 @@
         </div>
         <div class="display-flex">
             <div class="btn textbox-left-home">
-                <a class="back-btn" href="../index.php"> <span class="tr" Key="backBtn">უკან დაბრუნება</span></a>
+                <a class="back-btn" href="../javaxeti.php"> <span class="tr" Key="backBtn">უკან დაბრუნება</span></a>
             </div>
             <div class="btn textbox-right-home">
                 <a class="regions-btn" href="../list.php"> <span class="tr" Key="regionsBtn">რეგიონების შედარება</span></a>
@@ -452,19 +452,7 @@
         </table>
 
         <div class="map-box">
-            <svg id="svg4380" xmlns="http://www.w3.org/2000/svg" viewBox="" version="1.1">
-
-                <g id="layer5">
-
-                    <a xlink:href="../javaxeti.php" class="text-decoration-none">
-                        <path id="group5-zoomed" class="nonactive" data-clicked="false" data-scale="2.15" data-trans-x="-660" data-trans-y="-280" d="m 978.8,673.99 c 1.264,5.6852 0.26581,13.664 7.4565,9.2877 4.9546,1.9937 12.108,-0.23371 13.663,6.3583 3.1415,8.0597 15.886,0.63577 18.247,8.5905 -0.5446,5.6665 -0.3881,9.8902 6.5034,12.753 1.362,5.0943 -0.2367,9.4325 3.3267,14.423 2.3429,-1.5904 8.531,0.7567 11.754,-0.92962 3.8305,0.60466 2.1321,2.8207 1.4456,5.0899 0.67,2.9688 2.5333,6.3458 6.2779,6.3535 2.2882,0.26511 5.4217,-1.4538 4.7912,1.8535 2.4409,1.4761 4.55,2.974 7.5271,3.6036 2.3584,1.7436 4.8773,3.5618 7.9266,2.4944 3.8657,1.5306 7.8768,-2.3739 11.243,0.65229 3.0921,1.4813 3.7095,-4.3062 6.4442,-0.95905 6.2595,2.6169 12.061,5.1081 18.469,7.2597 6.0524,-1.333 5.086,1.4721 6.6196,6.8985 -2.7862,2.7925 -6.7816,4.5184 -5.8899,9.1385 2.101,3.7916 -0.1819,3.2552 -1.6132,3.5118 -4.4508,0.0621 -6.9813,2.9284 -10.643,3.7503 -1.4716,1.5601 -2.764,4.5634 -1.3437,6.7504 -0.9329,-0.97279 -4.4904,1.3638 -6.1938,-1.2014 -4.304,-1.4852 -6.2622,-5.777 -11.813,-4.2563 -3.3256,-1.8585 -6.7808,-2.0337 -9.9998,-2.043 -2.7469,3.6774 -5.031,7.4324 -4.2992,12.502 -0.3865,2.9976 -8.8287,-0.40577 -11.955,2.0938 -4.517,0.10962 -5.3874,3.1953 -5.6894,7.2935 1.0506,3.6733 -6.9957,2.3652 -9.4552,4.5726 -3.6055,2.2355 -12.299,1.8086 -13.004,5.4372 2.6947,2.5779 0.6757,5.8334 -1.8425,5.901 -2.4976,0.92492 -4.4856,0.0798 -6.5992,1.7955 -2.1825,1.9798 -4.4364,3.6086 -5.5947,6.3066 -0.866,-1.9374 -2.533,3.7886 -3.606,-0.21676 -0.7434,-4.3211 -1.9063,-9.8382 -3.7069,-14.714 -6.1683,0.20711 -8.8743,-6.4846 -15.135,-6.5841 -6.0461,-4.104 -8.601,1.1953 -10.867,-5.6347 -3.5534,0.0829 -5.8126,-5.0709 -8.8966,-0.58783 -0.68595,-4.7188 -6.8985,-6.0933 -6.8418,-11.397 -1.033,-3.4864 -10.928,-5.5469 -8.5101,-0.15994 -4.9802,1.0721 -9.5587,1.9616 -13.441,4.4941 -3.2986,-0.81494 -6.599,-1.441 -7.59,-4.3537 -0.27494,-2.6357 -0.99128,-5.6802 -4.0499,-6.5958 -1.9921,-0.24227 -1.628,-4.6212 -4.0822,-5.2258 -0.42154,-1.2463 0.87755,-5.0429 -1.8419,-5.3546 -1.3345,-5.1106 -5.6978,3.7052 -9.9238,-0.15226 -4.0701,-0.31071 2.0205,-2.1981 1.8163,-2.3673 1.2632,-1.0875 1.2559,-2.85 2.3321,-4.7933 0.59212,-2.2459 -3.1328,-6.1323 -3.0175,-9.5333 -3.7833,-2.3761 -10.774,-2.6523 -12.873,-7.5352 -1.8714,-3.2714 -2.3848,-6.321 -6.0254,-8.1739 -3.966,-5.8584 -1.0143,-4.1782 5.8456,-6.2008 6.087,-3.1588 13.933,-3.2724 16.916,-9.9398 6.5402,-2.5344 15.535,-2.4409 22.01,-6.2305 2.3632,-5.5288 8.9456,-4.2725 13.885,-6.5313 5.5826,2.208 10.244,-3.2748 12.266,-6.8716 5.6085,0.71619 9.1051,-3.8148 14.308,-4.4047 5.1524,1.843 9.6545,-6.4232 4.9355,-8.8155 3.0528,-3.869 -3.0278,-4.8202 0.33578,-7.4019 z" id="path6846" />
-                        <text id="svgRegions" x="935" y="751">
-                            ბორჯომი
-                        </text>
-                    </a>
-                   
-                </g>
-            </svg>
+            <img src="maps/borjomi.svg" alt="borjomiSVG" id="borjomiSVG">
         </div>
 
         <table class="machveneblebi-right">
