f8d064ebb8e9a5fcb1dbdf482e3a06a571e85133
nikolozi2001 <mr.qachibaia44@gmail.com>
2024-12-26 12:06:56 +0400

    fix: update Khoni references and improve meta tags for consistency

--- imereti/khoni.php
+++ imereti/khoni.php
@@ -11,13 +11,13 @@ include "../config.php";
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta property="og:url" content="http://www.geostat.ge/regions/" />
     <meta property="og:type" content="website" />
-    <meta property="og:title" content="<?php echo $lang['ხონიtitlename'] ?>" />
-    <meta property="og:description" content="<?php echo $lang['ხონიtitlename'] ?>" />
+    <meta property="og:title" content="<?php echo $lang['khonititlename'] ?>" />
+    <meta property="og:description" content="<?php echo $lang['khonititlename'] ?>" />
     <meta property="og:image" content="http://www.geostat.ge/regions/images/regionsbanner1.png" />
     <meta property="og:image:secure_url" content="http://www.geostat.ge/regions/images/regionsbanner1.png" />
     <meta property="og:image:width" content="740" />
     <meta property="og:image:height" content="450" />
-    <title class="tr" Key="PAGE_TITLE"><?php echo $lang['ხონიtitlename'] ?></title>
+    <title class="tr" Key="PAGE_TITLE"><?php echo $lang['khonititlename'] ?></title>
 
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
     <script src="https://code.jquery.com/jquery-3.2.1.min.js" integrity="sha256-hwg4gsxgFZhOsEEamdOYGBf13FyQuiTwlAQgxVSNgt4=" crossorigin="anonymous"></script>
@@ -955,7 +955,7 @@ include "../config.php";
             <div class="col-12 col-md-6 d-flex flex-column justify-content-center align-items-center" style="height: 100%;">
                 <!-- Center content goes here. Adjust height if needed or add other content. -->
                 <div class="w-100 text-center p-4 rounded" style="height: 100%;">
-                    <img src="<?php echo (isset($_GET['lang']) && $_GET['lang'] == 'en') ? 'maps/ხონიen.svg' : 'maps/ხონი.svg'; ?>" alt="ხონიSVG" id="ხონიSVG">
+                    <img src="<?php echo (isset($_GET['lang']) && $_GET['lang'] == 'en') ? 'maps/khonien.svg' : 'maps/khoni.svg'; ?>" alt="khoniSVG" id="khoniSVG">
                 </div>
             </div>
 
@@ -971,7 +971,7 @@ include "../config.php";
     <script src="../script.js"></script>
     <script type="text/javascript" src="../lang.js"></script>
     <script>
-        localStorage.setItem('municipal', 'ხონი');
+        localStorage.setItem('municipal', 'Khoni');
     </script>
     <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.7/dist/umd/popper.min.js"></script>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.min.js"></script>
